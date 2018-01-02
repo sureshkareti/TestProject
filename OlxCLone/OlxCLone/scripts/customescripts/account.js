e604c4f69558f11dda634fa8eaecee2a7f17b249
suresh.k <suresh.k@DESKTOP-CUB072>
2018-01-02 18:18:18 +0530

    partial comit on account

---
+++ OlxCLone/OlxCLone/scripts/customescripts/account.js
@@ -0,0 +1,17 @@
+﻿
+$(document).ready(function () {
+
+    $('#frmChangePassword').bootstrapValidator({
+
+        fields: {
+            txtConfirmPasscode: {
+                validators: {
+                    identical: {
+                        field: 'txtPasscode',
+                        message: 'The password and its confirm are not the same'
+                    }
+                }
+            }
+        }
+    });
+});
