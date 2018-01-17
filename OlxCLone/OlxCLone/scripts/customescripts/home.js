c13ddf6e4bae39b94988394c6fd3e957f3446e5b
suresh.k <suresh.k@DESKTOP-CUB072>
2018-01-17 18:23:29 +0530

    comit on all changes

---
+++ OlxCLone/OlxCLone/scripts/customescripts/home.js
@@ -0,0 +1,26 @@
+﻿
+$(document).ready(function () {
+    var urlParams = new URLSearchParams(window.location.search);
+    var requestedItem = urlParams.get('rd');
+    if (requestedItem != null) {
+        $('.categoryId li').removeClass('active');
+        $("[data-link='" + requestedItem + "']").addClass('active');
+        $('.category-link').css('display', 'none');
+        $('#' + requestedItem).css('display', 'block');
+    }
+
+
+    console.log(); // true
+    
+});
+
+
+
+
+$('.categoryId li').on('click', function (e) {
+    e.preventDefault();
+    $('.categoryId li').removeClass('active');
+    $(this).addClass('active');
+    $('.category-link').css('display', 'none');  
+    $('#'+$(this).attr("data-link")).css('display', 'block');
+});
